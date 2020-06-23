88b37e917c0945bef781945ec207a4665c00aa06
Johannes Schildgen <yosi@schildix.de>
2020-06-23 20:11:06 +0200

    Finished chapter 7

---
+++ code/Zinsrechner/src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("zinsrechner.fxml"));
+        primaryStage.setTitle("Zinsrechner");
+        primaryStage.setScene(new Scene(root, 300, 190));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
